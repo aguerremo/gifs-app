583b37f39499816f6c0e898dd29df080fbd2f56d
Alfonso <agm0121@alu.medac.es>
2025-11-30 00:04:39 +0100

    feat: Create `gifs-side-menu` component and refactor side menu options to be dynamic and router-driven.

--- src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.ts
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -1,9 +1,36 @@
+import { routes } from './../../../../app.routes';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+  interface MenuOptions {
+    icon: string,
+    label: string,
+    subLabel: string,
+    route: string,
+  }
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GifsSideMenuOptions { }
+export class GifsSideMenuOptions {
+
+  menuOptions:MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Popular Gifs',
+      route: '/dashboard/trending'
+    },
+       {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      subLabel: 'Search Gifs',
+      route: '/dashboard/search'
+    }
+  ]
+
+
+}
--- src/app/gifs/pages/dashboard/dashboard.ts
+++ src/app/gifs/pages/dashboard/dashboard.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GifsSideMenuHeader } from "../../components/gifs-side-menu-header/gifs-side-menu-header";
-import { GifsSideMenuOptions } from "../../components/gifs-side-menu-options/gifs-side-menu-options";
+import { GifsSideMenuHeader } from "../../components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header";
+import { GifsSideMenuOptions } from "../../components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options";
+import { GifsSideMenu } from "../../components/gifs-side-menu/gifs-side-menu";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet, GifsSideMenuHeader, GifsSideMenuOptions],
+  imports: [RouterOutlet, GifsSideMenuHeader, GifsSideMenuOptions, GifsSideMenu],
   templateUrl: './dashboard.html',
 })
 export default class Dashboard { }
